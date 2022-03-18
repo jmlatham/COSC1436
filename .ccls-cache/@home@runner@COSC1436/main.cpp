470e066a24e0e6d6901456906368619884c77f77
JMLatham <jmlatham@yahoo.com>
2022-03-18 16:05:33 +0000

    added code to show two different ways to get input from the user.

--- .ccls-cache/@home@runner@COSC1436/main.cpp
+++ .ccls-cache/@home@runner@COSC1436/main.cpp
@@ -8,5 +8,21 @@ int main() {
   std::cout << "*           Author: JML          *\n\n";
   std::cout << "*   Due Date: Friday, March 21   *\n\n";
   std::cout << "**********************************\n";
+
+  char ch;
+  std::cin.get(ch);
+  while(ch != '\n')
+  {
+    if(ch != '\n'){
+      std::cout<<"\ninside loop\n";
+    }
+    std::cout<<ch;
+    std::cin.get(ch);
+  }
+  std::cout << "\n";
+  int g;
+  std::cin>>g;
+  std::cout << "Output is: "<< g;
+  std::cout << "\n";
   return 0;
 }
