e99df419b925c6405c254afaeed91637dd89fafc
mlatham <>
2022-03-18 14:42:09 +0000

    Added the code to print the requested information

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello World!\n";
+  // Write your main here
+  std::cout << "**********************************\n\n";
+  std::cout << "*    Programming Assignment 1    *\n\n";
+  std::cout << "*     Computer Programming I     *\n\n";
+  std::cout << "*           Author: JML          *\n\n";
+  std::cout << "*   Due Date: Friday, March 21   *\n\n";
+  std::cout << "**********************************\n";
+  return 0;
 }
