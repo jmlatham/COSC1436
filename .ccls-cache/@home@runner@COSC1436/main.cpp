6441d744c4583f292263244768ecef0ebdb5d732
JMLatham <jmlatham@yahoo.com>
2022-03-18 19:34:34 +0000

    refactored the code to use functions

--- .ccls-cache/@home@runner@COSC1436/main.cpp
+++ .ccls-cache/@home@runner@COSC1436/main.cpp
@@ -1,4 +1,39 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  bool quit = false;
+  while (!quit){
+    int inches = 100;
+    cout << "Enter the total number of inches: ";
+    cin >> inches;
+    cout << inches << " inch(es) = ";
+    cout << inches / 12 << " feet (foot) and ";
+    cout << inches % 12 << " inch(es) = " << endl;
+
+    string firstName;
+    string lastName;
+    int age;
+    double weight;
+
+    cout << "\nEnter first name, last name, age, and weight separated by spaces:";
+    cin >> firstName >> lastName >> age >> weight;
+    cout << "\nName: " << firstName << " " << lastName << "\nAge:" << age << "\nWeight:" << weight << "\n\n";
+    char again = 'n';
+    cout << "Run again? Y for 'Yes' and N for 'No' : ";
+    cin >> again;
+      if(again == 'n' || again == 'N'){
+        quit = true;
+      }
+    }
+  cout << endl;
+  return 0;
+}
+
+
+/*
+#include <iostream>
 
 int main() {
   // Write your main here
@@ -26,3 +61,4 @@ int main() {
   std::cout << "\n";
   return 0;
 }
+*/
--- main.cpp
+++ main.cpp
@@ -1,4 +1,69 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+void inchesTestFunction();
+void infoTestFunction();
+void variableTestFunction();
+
+int main() {
+  bool quit = false;
+  while (!quit){
+
+    //inchesTestFunction();
+
+    //infoTestFunction();
+
+    variableTestFunction();
+    
+    char again = 'n';
+    cout << "Run again? Y for 'Yes' and N for 'No' : ";
+    cin >> again;
+      if(again == 'n' || again == 'N'){
+        quit = true;
+      }
+    }
+  cout << endl;
+  return 0;
+}
+
+void inchesTestFunction() {
+  int inches = 100;
+    cout << "Enter the total number of inches: ";
+    cin >> inches;
+    cout << inches << " inch(es) = ";
+    cout << inches / 12 << " feet (foot) and ";
+    cout << inches % 12 << " inch(es) = " << endl;
+}
+
+void infoTestFunction() {
+  string firstName;
+    string lastName;
+    int age;
+    double weight;
+
+    cout << "\nEnter first name, last name, age, and weight separated by spaces:";
+    cin >> firstName >> lastName >> age >> weight;
+    cout << "\nName: " << firstName << " " << lastName << "\nAge:" << age << "\nWeight:" << weight << "\n\n";
+}
+
+void variableTestFunction(){
+
+  int a, b;
+  double z;
+  char ch;
+
+  cin >> ch;
+  cin >> a >> b;
+  cin >> z;
+
+  cout << "\na = " << a << "\nb = " << b << "\nz = " << z << "\nch = " << ch << endl;
+  
+}
+
+
+/*
+#include <iostream>
 
 int main() {
   // Write your main here
@@ -26,3 +91,4 @@ int main() {
   std::cout << "\n";
   return 0;
 }
+*/
